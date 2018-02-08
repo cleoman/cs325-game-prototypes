7f8fcbd88a971f764c84da20bc93b643fd2d3ac9
cleoman <cmonty260@gmail.com>
2018-02-07 23:23:41 -0500

    basic functionality

--- da1/js/main.js
+++ da1/js/main.js
@@ -15,12 +15,19 @@ window.onload = function() {
     function preload() {
         // Load an image and call it 'logo'.
         game.load.image( 'logo', 'assets/phaser.png' );
-        game.load.image( 'grasstile', 'assets/grass1.jpg' );
+        game.load.image( 'dirttile', 'assets/dirt1.jpg' );
         game.load.spritesheet( 'mage', 'assets/mage.png', 16, 16 );
+        game.load.image( 'grass', 'assets/grass1.jpg' );
+        game.load.audio( 'ding', 'assets/dingCling-positive.ogg');
 
     }
 
     var bouncy;
+    let pc;
+    let grass;
+    let score = 0;
+    let text;
+    let ding;
 
     function create() {
         // Create a sprite at the center of the screen using the 'logo' image.
@@ -40,50 +47,99 @@ window.onload = function() {
         // var text = game.add.text( game.world.centerX, 15, "Build something amazing.", style );
         // text.anchor.setTo( 0.5, 0.0 );
 
-        let tileSprite = game.add.tileSprite(0, 0, 800, 600, 'grasstile');
-        this.pc = game.add.sprite(game.width /2, game.height /2, 'mage');
-        this.game.physics.arcade.enable(this.pc);
-        this.pc.scale.setTo(2,2);
-        this.pc.goingUp = false;
-        this.pc.goingDown = false;
-        this.pc.goingLeft = false;
-        this.pc.goingRight = false;
-        this.pc.idleFrame = 0;
 
-        let moveDown = this.pc.animations.add("moveDown", [0, 1, 2, 3]);
-        let moveUp = this.pc.animations.add("moveUp", [4, 5, 6, 7]);
-        let moveRight = this.pc.animations.add("moveRight", [8,9,10,11]);
-        let moveLeft = this.pc.animations.add("moveLeft", [12,13,14,15]);
 
+        // dirt bg
+        let tileSprite = game.add.tileSprite(0, 0, 800, 600, 'dirttile');
+
+        // sounds
+        ding = game.add.audio('ding');
+
+        // create the "grass" or diggable tiles
+        grass = game.add.group();
+        grass.createMultiple(450, 'grass',0,true);
+        grass.align(25, 19, 32, 32);
+        grass.x = 0;
+        grass.y = 0;
+        console.log(grass);
+
+        // create the player character
+        pc = game.add.sprite(game.width /2, game.height /2, 'mage');
+        this.game.physics.arcade.enable(pc);
+        pc.body.collideWorldBounds = true;
+        pc.scale.setTo(2,2);
+        pc.goingUp = false;
+        pc.goingDown = false;
+        pc.goingLeft = false;
+        pc.goingRight = false;
+        pc.doAction = false;
+        pc.actionJustDone = false;
+        pc.idleFrame = 0;
+
+        // define player character animations
+        let moveDown = pc.animations.add("moveDown", [0, 1, 2, 3]);
+        let moveUp = pc.animations.add("moveUp", [4, 5, 6, 7]);
+        let moveRight = pc.animations.add("moveRight", [8,9,10,11]);
+        let moveLeft = pc.animations.add("moveLeft", [12,13,14,15]);
+
+        // score display
+        text = game.add.text(0,0, "Score: " + score, {
+          font: "32px Arial",
+          fill: "#f4f000",
+          align: "center"
+        });
+
+        let directions = game.add.text(0,40, "WASD to move\nE to dig", {
+          font: "12px Arial",
+          fill: "#f4f000",
+          align: "center"
+        });
+
+
+
+        // keyboard triggers
         let upKeyPressed = game.input.keyboard.addKey(Phaser.Keyboard.W);
         let downKeyPressed = game.input.keyboard.addKey(Phaser.Keyboard.S);
         let leftKeyPressed = game.input.keyboard.addKey(Phaser.Keyboard.A);
         let rightKeyPressed = game.input.keyboard.addKey(Phaser.Keyboard.D);
+        let actionKeyPressed = game.input.keyboard.addKey(Phaser.Keyboard.E);
 
+        // handle keyboard triggers
         upKeyPressed.onDown.add(function() {
-          this.pc.goingUp = true;
+          pc.goingUp = true;
         }, this);
         upKeyPressed.onUp.add(function() {
-          this.pc.goingUp = false;
+          pc.goingUp = false;
         }, this);
         downKeyPressed.onDown.add(function() {
-          this.pc.goingDown = true;
+          pc.goingDown = true;
         }, this);
         downKeyPressed.onUp.add(function() {
-          this.pc.goingDown = false;
+          pc.goingDown = false;
         }, this);
         leftKeyPressed.onDown.add(function() {
-          this.pc.goingLeft = true;
+          pc.goingLeft = true;
         }, this);
         leftKeyPressed.onUp.add(function() {
-          this.pc.goingLeft = false;
+          pc.goingLeft = false;
         }, this);
         rightKeyPressed.onDown.add(function() {
-          this.pc.goingRight = true;
+          pc.goingRight = true;
         }, this);
         rightKeyPressed.onUp.add(function() {
-          this.pc.goingRight = false;
+          pc.goingRight = false;
         }, this);
+        actionKeyPressed.onDown.add(function() {
+          pc.doAction = true;
+
+          console.log("PRESSED A BUTTON");
+        }, this);
+        actionKeyPressed.onUp.add(function() {
+          pc.doAction = false;
+          pc.actionJustDone = false;
+        })
+
+
 
     }
 
@@ -94,30 +150,51 @@ window.onload = function() {
         // This function returns the rotation angle that makes it visually match its
         // new trajectory.
         //bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, game.input.activePointer, 500, 500, 500 );
-        if(this.pc.goingRight){
-          this.pc.animations.play("moveRight", 10, true);
-          this.pc.idleFrame = 8;
-          this.pc.body.velocity.x = 100;
+        if(pc.doAction && !pc.actionJustDone ){
+          console.log("AM HERE");
+          playerAction();
         }
-        else if(this.pc.goingLeft){
-          this.pc.animations.play("moveLeft", 10, true);
-          this.pc.idleFrame = 12;
-          this.pc.body.velocity.x = -100;
+        if(pc.goingRight){
+          pc.animations.play("moveRight", 10, true);
+          pc.idleFrame = 8;
+          pc.body.velocity.x = 200;
+          pc.body.velocity.y = 0;
         }
-        else if(this.pc.goingUp){
-          this.pc.animations.play("moveUp", 10, true);
-          this.pc.idleFrame = 4;
-          this.pc.body.velocity.y = -100;
+        else if(pc.goingLeft){
+          pc.animations.play("moveLeft", 10, true);
+          pc.idleFrame = 12;
+          pc.body.velocity.x = -200;
+          pc.body.velocity.y = 0;
         }
-        else if(this.pc.goingDown){
-          this.pc.animations.play("moveDown", 10, true);
-          this.pc.idleFrame = 0;
-          this.pc.body.velocity.y = 100;
+        else if(pc.goingUp){
+          pc.animations.play("moveUp", 10, true);
+          pc.idleFrame = 4;
+          pc.body.velocity.y = -200;
+          pc.body.velocity.x = 0;
+        }
+        else if(pc.goingDown){
+          pc.animations.play("moveDown", 10, true);
+          pc.idleFrame = 0;
+          pc.body.velocity.y = 200;
+          pc.body.velocity.x = 0;
         }
         else {
-          this.pc.frame = this.pc.idleFrame;
-          this.pc.body.velocity.x = 0;
-          this.pc.body.velocity.y = 0;
+          pc.frame = pc.idleFrame;
+          pc.body.velocity.x = 0;
+          pc.body.velocity.y = 0;
         }
+    }
+
+    function playerAction() {
+        let closestTile = grass.getClosestTo(pc);
+        console.log(closestTile);
+        if(closestTile.visible === true){
+          closestTile.visible = false;
+          score++;
+          text.setText("Score: " + score);
+          ding.play();
+          pc.actionJustDone = true;
+        }
+
     }
 };
