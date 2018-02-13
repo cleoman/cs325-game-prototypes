f24f08667ecd1049654c456f471bbe9bcf2d23db
cleoman <cmonty260@gmail.com>
2018-02-12 21:16:56 -0500

    digital assignment 2 created

---
+++ da2/assets/chickengame.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="chickengame" tilewidth="32" tileheight="32" tilecount="48" columns="8">
+ <image source="chickengame.gif" width="256" height="192"/>
+</tileset>
